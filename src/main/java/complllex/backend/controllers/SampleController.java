a8e9aeccf2e1d8752052b3687f0605fe842590ae
Complllex <minor6255@gmail.com>
2025-04-13 20:23:41 +0300

    Lab 5

--- src/main/java/complllex/backend/controllers/SampleController.java
+++ src/main/java/complllex/backend/controllers/SampleController.java
@@ -3,6 +3,9 @@ package complllex.backend.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
 @RestController
 @RequestMapping("/api/v1")
 public class SampleController {
---
+++ src/main/java/complllex/backend/repositories/CountryRepository.java
@@ -0,0 +1,10 @@
+package complllex.backend.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import complllex.backend.models.Country;
+
+@Repository
+public interface CountryRepository  extends JpaRepository<Country, Long>
+{
+
+}
